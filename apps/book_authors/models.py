558df1f338aed4082a99c3bd0cf287d997009c96
dshayne07 <davidshayne2007@gmail.com>
2018-01-24 14:11:34 -0500

    Added likes/books

--- apps/book_authors/models.py
+++ apps/book_authors/models.py
@@ -11,9 +11,18 @@ class Author(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 class Book(models.Model):
     name = models.CharField(max_length=255)
     desc = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    users = models.ForeignKey(User, related_name="uploader")
+    liked_users = models.ManyToManyField(User, related_name="liked_books")
     authors = models.ManyToManyField(Author, related_name="books")
